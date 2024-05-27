77b08f266993e1ce8a1fd966636f26f519095c45
Riki Awaludin <rikiawaludin17@gmail.com>
2024-05-27 14:37:02 +0700

    sudah ada pemenangnya

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
 
 function Square( {value, onSquareClick} ) {
@@ -8,14 +9,30 @@ function Square( {value, onSquareClick} ) {
 export default function Board() {
 
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
+
+    if(squares[i] || calculateWinner(squares)) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    nextSquares[i] = xIsNext ? 'X' : 'O';
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status = '';
+
+  if (winner) {
+    status = 'Winner ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext? 'X': 'O');
   }
 
   return (
+    <>
+    <div className='status'>{status}</div>
     <div className="board">
       <Square value= {squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value= {squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -27,5 +44,29 @@ export default function Board() {
       <Square value= {squares[7]} onSquareClick={() => handleClick(7)}/>
       <Square value= {squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
+    </>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
+
+  for (let i=0; i < lines.length; i++){
+    const [a, b, c] = lines[i];
+
+    if(squares[a] && squares[a] === squares[b] && squares[c] ){
+      return squares[a];
+    }
+  }
+
+  return false;
 }
